6200da0ed77abf9ed1d40ab83dc7555c17589446
Edward <edwardsitar4@gmail.com>
2021-03-09 12:43:26 -0500

    added classes

---
+++ Gun.java
@@ -0,0 +1,50 @@
+
+
+public class Gun extends Evidence {
+    public String gunType;
+    public String model;
+    public Bullet bulletType;
+    public int yearMade;
+
+    public Gun() {
+
+    }
+
+    public String getGunType() {
+
+    }
+
+    public String getModel() {
+
+    }
+
+    public Bullet getBulletType() {
+
+    }
+
+    public int getYearMade() {
+
+    }
+
+    public void setGunType(String type) {
+
+    }
+
+    public void setModel(String model) {
+
+    }
+
+    public void setBulletType(Bullet b) {
+
+    }
+
+    public void setYearMade(int year) {
+
+    }
+
+    public String toString() {
+        
+    }
+
+
+}
---
+++ LawEnforcementUser.java
@@ -0,0 +1,34 @@
+
+
+public class LawEnforcementUser implements Comparable{
+    protected String username;
+    protected String password;
+    public String firstName;
+    public String lastName;
+    protected int clearanceLevel;
+    protected UUID LawEnforcementID;
+
+    public void LawEnforcementUser(String username, String password, int clearance) {
+
+    }
+
+    public String getUsername() {
+
+    }
+
+    public int getClearanceLevel() {
+
+    }
+
+    public String getName() {
+
+    }
+
+    public boolean passwordCorrect(String password) {
+
+    }
+
+    public int compareTo(LawEnforcementUser officer) {
+
+    }
+}
