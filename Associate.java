20a8d62cd69b18edf01817e1a315761820f5af92
mercybar <77426515+mercybar@users.noreply.github.com>
2021-03-10 11:40:31 -0500

    Add files via upload

---
+++ Associate.java
@@ -0,0 +1,21 @@
+import java.util.UUID;
+
+public class Associate extends Person{
+    private String relationship;
+    private UUID relationshipID;
+
+
+    public Associate() {
+        return;
+    }
+
+    public void addToDataBase(Person p) {
+        return;
+    }
+
+    public String toString(){
+        return "";
+    }
+
+
+}
---
+++ Suspect.java
@@ -0,0 +1,11 @@
+public class Suspect extends PersonOfInterest {
+
+    public Suspect(){
+        return;
+    }
+
+    public String toString(){
+        return "";
+    }
+    
+}
