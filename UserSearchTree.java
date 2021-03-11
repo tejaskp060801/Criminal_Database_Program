08215dc8bad26e6c35b55c7b09c8ab54275bb991
Edward <edwardsitar4@gmail.com>
2021-03-11 08:28:06 -0500

    added userST stub

---
+++ UserSearchTree.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class userSearchTree {
+    private UserSearchTree userSearchTree;
+    private BinarySearchTree<LawEnforcementUser> users;
+
+    private UserSearchTree() {
+
+    }
+
+    public UserSearchTree getUserSearchTree() {
+        return this.userSearchTree;
+    }
+
+    public boolean Login (String username, String pw) {
+        return true;
+    }
+
+    public LawEnforcementUser addUser(String username, String pw, double clearance) {
+        return new LawEnforcementUser();
+    }
+
+    public LawEnforcementUser search(String username) {
+        return new LawEnforcementUser();
+    }
+
+    public void printUserNamesInOrder() {
+        return;
+    }
+
+
+}
