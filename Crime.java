24064cbcc68132f1d5640af304bb87143dbcafb6
tejaskp <tejaskp060801@gmail.com>
2021-03-10 23:37:06 -0500

    made the criminal file and the stub

---
+++ Crime.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Crime {
+    protected UUID caseNumber; 
+    protected String title; 
+    protected boolean isSolved; 
+    protected ArrayList<Person> People; 
+    protected ArrayList<TypeOfCrime> typeOfCrime; 
+    protected String location; 
+    protected String date;
+    protected ArrayList<Evidence> evidence; 
+    protected ArrayList<LawEnforcementUser> workingOnCase; 
+    protected String crimeDescription; 
+
+    public Crime() {
+        this.caseNumber = caseNumber; 
+        this.title = title; 
+        this.People = People; 
+        this.crimeDescription = crimeDescription; 
+        this.date = date; 
+        this.evidence = evidence; 
+        this.isSolved = isSolved; 
+        this.location = location; 
+        this.typeOfCrime = typeOfCrime; 
+        this.workingOnCase = workingOnCase; 
+    }
+    
+    public void deleteCrime() {
+
+    }
+    public void crimeSolved() {
+        
+    }
+    public String getTitle() {
+
+    }
+    public ArrayList<Person> getPOIArray() {
+        return People; 
+    }
+    public ArrayList<Criminal> getCriminalArray() {
+         
+    }
+    public ArrayList<UUID> getWorkingOnCaseArrayList() {
+
+    }
+    public String getCrimeDescription() {
+        return crimeDescription; 
+    }
+    public String getDate() {
+        return date; 
+    }
+    public ArrayList<TypeOfCrime> getTypeOfCrime() {
+        return typeOfCrime; 
+    }
+    public void addInformation() {
+
+    }
+    public void editInformation(){
+        
+    }
+    public String toString() {
+        return ""; 
+    }
+}
