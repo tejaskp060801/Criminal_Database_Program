b289c10c0de905c8415dd27b1e23b44527bbc609
tejaskp <tejaskp060801@gmail.com>
2021-03-22 15:48:28 -0400

    made changes to help with the UI

--- FamilyMember.java
+++ FamilyMember.java
@@ -8,10 +8,10 @@ public class FamilyMember extends Person{
     public FamilyMember(String firstName, String lastName, int age, String gender, String race, UUID id, String address,
     String profession, double height, double weight, String skinCol, String natHairCol, String unNatHairCol,
     String clothing, boolean hasTattoo, String tatDesc, double shoeSize, boolean isUsCitizen,
-    String org, String relationship, UUID id) {
+    String org, String relationship, UUID relationshipID) {
         super(firstName, lastName, age, gender, race, id, address, profession, height, weight, skinCol, natHairCol, unNatHairCol, clothing, hasTattoo, tatDesc, shoeSize, isUsCitizen, org); 
         this.setRelationship(relationship);
-        this.setRelationshipID(id);
+        this.setRelationshipID(relationshipID);
     }
     public String getRelationship() {
         return relationship; 
@@ -22,7 +22,7 @@ public class FamilyMember extends Person{
     public void setRelationship(String relationship){
         this.relationship = relationship; 
     }
-    public void setRelationshipID(UUID id){
+    public void setRelationshipID(UUID relationshipID){
         this.relationshipID = id; 
     }
     public String toString() {
