af73041ad9d9d91f6d54028b2a9e4f2ce670b869
Mercy Madhurima Barigala <mercymadhurimabarigala@Mercys-MacBook-Pro.local>
2021-03-23 05:01:42 -0400

    added functionality

--- Crime.java
+++ Crime.java
@@ -5,12 +5,12 @@ public class Crime {
     protected UUID casenumber; 
     protected String title; 
     protected boolean isSolved; 
-    protected ArrayList<Person> people; //array
-    protected ArrayList<TypeOfCrime> typeOfCrime;  //array
+    protected ArrayList<Person> people; 
+    protected ArrayList<TypeOfCrime> typeOfCrime;  
     protected String location; 
     protected String date;
-    protected ArrayList<Evidence> evidence; //array
-    protected ArrayList<LawEnforcementUser> workingoncase; //array
+    protected ArrayList<Evidence> evidence; 
+    protected ArrayList<LawEnforcementUser> workingoncase; 
     protected String crimedescription; 
 
     public Crime(String title, boolean isSolved, ArrayList<TypeOfCrime> felonyType, ArrayList<Person> people, String location, String date, ArrayList<Evidence> evidenceList, ArrayList<String> officers, String description) {
@@ -25,57 +25,72 @@ public class Crime {
         this.typeOfCrime = typeOfCrime; 
         this.workingoncase = workingoncase; 
     }
-    
+    /*
+    deleteCrime method isnt needed cause we have a removeCrime method in CrimeList anyways
+   
 
     public void deleteCrime(Crime crime) {
-        
+        return CrimeList.remove(crime);
     }
-    public void crimeSolved() {
-        
+    */
+     
+    
+
+    public boolean crimeSolved() {
+        return true;
     }
+
     public String getTitle(String title) {
         return title; 
     }
+
     public ArrayList<Person> getPOIArray() {
          ArrayList<Person> people = new ArrayList<Person>();
         return people; 
 
     }
+
     public static ArrayList<Criminal> getCriminalArray() {
          ArrayList<Criminal> criminalswoo = new ArrayList<Criminal>();
          return criminalswoo;
     }
+
     public ArrayList<UUID> getWorkingOnCaseArrayList() {
         ArrayList<UUID> workingOnCase= new ArrayList<UUID>();
         return workingOnCase; 
     }
+
     public String getCrimeDescription() {
         return crimedescription; 
     }
+
     public String getDate() {
         return date; 
     }
+
     public ArrayList<TypeOfCrime> getTypeOfCrime() {
         ArrayList<TypeOfCrime> typeOfCrimes = new ArrayList<TypeOfCrime>();
         return typeOfCrime; 
     }
-    /* do these bottom methods mean add information into like which specific attribute
-    *
-    *
-    */
-    public void addInformation() { //i dont understand this method 
 
+    //i dont these two are neccessary because the person class doesnt have one either
+
+    public void addInformation() {  
+        
+       
     }
-    public void editInformation(){ //i dont understand this one either
+
+    public void editInformation(){ 
         
     }
-    public String toString() { //this one
-        return "\n----------\n" + "Case Number: " + this.caseNumber +"\nTitle "
+
+    public String toString() { 
+        return "\n----------\n" + "Case Number: " + this.casenumber +"\nTitle "
         + this.title  + "\nSolved or Unsolved: " + this.isSolved +
-        "\nPeople" + this.People + " \nType of Crime" + this.typeOfCrime+ " Location\n" +
+        "\nPeople" + this.people + " \nType of Crime" + this.typeOfCrime+ " Location\n" +
         this.location + "\nDate: " + this.date + "\nEvidence: " + this.evidence + 
-        "\nLaw Enforcement User Working On Case: " + this.workingOnCase +
-        "\nDescription of Crime: " + this.crimeDescription + "\n----------\n"; 
+        "\nLaw Enforcement User Working On Case: " + this.workingoncase +
+        "\nDescription of Crime: " + this.crimedescription + "\n----------\n"; 
     }
 
     public UUID getcasenumber() {
