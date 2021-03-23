5cf72ce891cf0c3add072195af648e08a7e409a2
Tyler Barrett <tylerbarrett01@gmail.com>
2021-03-23 10:01:24 -0400

    running tests, ignore

--- DataBaseUI.java
+++ DataBaseUI.java
@@ -15,15 +15,8 @@ public class DataBaseUI {
 
 
     public static void main(String [] args) {
-        /*DataBaseUI ui = new DataBaseUI();
-        ui.run();*/
-        PersonList temp = PersonList.getPersonList();
-        ArrayList<Person> arr = temp.getPeople();
-        /*for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i));
-        }*/
-
-        JSONWriter.savePeople();
+        DataBaseUI ui = new DataBaseUI();
+        ui.run();
     }
 
     public void run() {
