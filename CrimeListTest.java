4b0f3a6b342e8e64024cffab2dd3c93552b897f3
Mercy Madhurima Barigala <mercymadhurimabarigala@Mercys-MacBook-Pro.local>
2021-04-11 17:37:23 -0400

    still doing test

--- CrimeListTest.java
+++ CrimeListTest.java
@@ -1,6 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.UUID;
 
-public class CrimeListTest {
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jdk.jfr.Timestamp;
+class CrimeListTest {
+
+    CrimeList crimeList = getCrimeList();
+    private ArrayList<Crime> crimes = crimeList.getCrimes();
+    ArrayList<Crime> tempList = crimes;
+
+    @BeforeEach
+    public void reset(){
+        crimes = tempList;
+    }
+
+   
    
     
 }
